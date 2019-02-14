cf2838cc66f70ea1329b22a4bb75cb9ba14505ee
Sarah Luning <31362940+soc33@users.noreply.github.com>
2019-02-14 08:23:16 -0500

    initial changes

---
+++ assets/javascript/app.js
@@ -0,0 +1,3 @@
+var name = document.getElementById("name");
+// window.onload = function () {$("#name").animate({margin: "0px"}, {speed: "70000ms"})};
+window.onscroll = function () {$("#header").animate({height: "-=150px"})};
