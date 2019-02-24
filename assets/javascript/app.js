71f65448684390334ff20ff8fd8b9347cad01550
soc33 <soc33@msn.com>
2019-02-24 16:13:45 -0500

    new thoughts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,20 @@
-var name = document.getElementById("name");
-// window.onload = function () {$("#name").animate({margin: "0px"}, {speed: "70000ms"})};
-window.onscroll = function () {$("#header").animate({height: "-=150px"})};
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    document.getElementById("header2").style.fontSize = "30px";
+    document.getElementById("header2").style.display = "fixed";
+    // document.getElementById("header").style.height = "50px";
+    document.getElementById("header1").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("subtitle").style.display = "none";
+    document.getElementById("symbol").style.display = "none";
+  } else {
+    document.getElementById("header1").style.display = "flex";
+    document.getElementById("header1").style.fontSize = "90px";
+    document.getElementById("header1").style.height = "1000px";
+    document.getElementById("title").style.display = "flex";    
+    document.getElementById("subtitle").style.display = "flex";    
+    document.getElementById("symbol").style.display = "flex";    
+  }
+}
